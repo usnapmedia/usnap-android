5edaa3691594dfae357a7258f4e0ae491f8b5805
Jingsi Lu <jlu@samsaodev.com>
2015-05-13 19:01:31 -0400

    Fixed settings view

--- app/src/main/java/com/samsao/snapzi/contest_page/ContestFragment.java
+++ app/src/main/java/com/samsao/snapzi/contest_page/ContestFragment.java
@@ -2,18 +2,17 @@ package com.samsao.snapzi.contest_page;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.samsao.snapzi.R;
-import com.samsao.snapzi.SnapziApplication;
 import com.samsao.snapzi.api.entity.Campaign;
 import com.samsao.snapzi.api.util.CustomJsonDateTimeDeserializer;
 import com.samsao.snapzi.util.StringUtil;
@@ -101,13 +100,6 @@ public class ContestFragment extends Fragment {
         mDate.setText(mDateString);
         mPrize.setText(mCampaignPrize);
 
-        Typeface fontText = Typeface.createFromAsset(SnapziApplication.getContext().getAssets(), "fonts/GothamHTF-Book.ttf");
-        mTitle.setTypeface(fontText);
-        mDescription.setTypeface(fontText);
-        mDate.setTypeface(fontText);
-        mPrize.setTypeface(fontText);
-        mRules.setTypeface(fontText);
-
         setupToolbar();
         return view;
     }
@@ -130,6 +122,17 @@ public class ContestFragment extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                getActivity().finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void setupToolbar() {
         if (mToolbar != null) {
             mListener.setSupportActionBar(mToolbar);
@@ -139,14 +142,16 @@ public class ContestFragment extends Fragment {
         mListener.getSupportActionBar().setTitle(StringUtil.getAppFontString(R.string.Contest));
     }
 
+    /**
+     * make up the Date from startDate and endDate
+     * @param startDate
+     * @param endDate
+     * @return
+     */
     private String composeDate(String startDate, String endDate) {
         DateTimeFormatter dateTimeFormatter = CustomJsonDateTimeDeserializer.getDateFormatter();
         DateTime mStartDate = dateTimeFormatter.parseDateTime(startDate);
         DateTime mEndDate = dateTimeFormatter.parseDateTime(endDate);
-        System.out.println(mStartDate.toString());
-        System.out.println(mEndDate.toString());
-        System.out.println(mDateFormat.print(mStartDate));
-        System.out.println(mDateFormat.print(mEndDate));
         return mDateFormat.print(mStartDate) + " - " + mDateFormat.print(mEndDate);
     }
 
